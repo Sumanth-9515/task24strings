6689728686a11711e0aace0d8d59c26fd3ea1e31
Sumanth-9515 <myakalasumanthreddy@gmail.com>
2024-10-03 18:18:21 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,88 @@
+var a="sumanth"
+ var r= a.slice(2,5)
+// var b=console.log(a.slice(2,5));
+console.log(r.toUpperCase());
+
+// 2
+var c="akashpatil"
+console.log(c.toUpperCase());
+console.log(c.length);
+
+//3
+var d="sumanth"
+console.log(d.charCodeAt(5));
+
+// 4
+var e="    sumanth"
+var f="reddy    "
+var m= e.trimStart();
+var n= f.trimEnd();
+var result= m.concat( " ",n)
+console.log(result);
+
+// 5
+var g="VAMSHI"
+var h="REDDY"
+var s=h.toLocaleLowerCase()
+var result1=g.concat(s)
+console.log(result1);
+
+
+// 6
+var o="    sumanthreddy"
+var n= o.trimStart();
+console.log(n.toLocaleUpperCase());
+
+//7
+var g="siddu"
+var h= g.charAt(4)
+console.log(h);
+console.log(h.toLocaleUpperCase());
+
+//8
+var j=""
+
+
+//9
+var input="     hello world      "
+var k=input.trimStart()
+var p=input.trimEnd()   //doubt
+var result2=k.concat(p)
+console.log(result2);
+
+//10
+var string1="HELLO"
+var string2="WORLD"
+var l1= string1.toLocaleLowerCase()
+var l2= string2.toLocaleLowerCase()
+// console.log(l1,l2);
+var result3= l1.concat(" ",l2)
+console.log(result3);
+ var i=result3.charAt(2)
+ console.log(i);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
